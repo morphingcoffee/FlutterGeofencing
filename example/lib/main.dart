026311312ac6e4ae123761b8b0b846259c25b06c
Morphing Coffee <morphingcoffee@gmail.com>
2021-11-16 11:26:51 +0000

    Dart sound null safety modifications

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -53,8 +53,9 @@ class _MyAppState extends State<MyApp> {
 
   static void callback(List<String> ids, Location l, GeofenceEvent e) async {
     print('Fences: $ids Location $l Event: $e');
-    final SendPort send =
-    IsolateNameServer.lookupPortByName('geofencing_send_port');
+    final SendPort? send =
+        IsolateNameServer.lookupPortByName('geofencing_send_port');
+    assert(send != null);
     send?.send(e.toString());
   }
 
@@ -65,100 +66,81 @@ class _MyAppState extends State<MyApp> {
     print('Initialization done');
   }
 
-  String numberValidator(String value) {
-    if (value == null) {
-      return null;
-    }
-    final num a = num.tryParse(value);
-    if (a == null) {
-      return '"$value" is not a valid number';
-    }
-    return null;
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter Geofencing Example'),
+        appBar: AppBar(
+          title: const Text('Flutter Geofencing Example'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Current state: $geofenceState'),
+              Center(
+                child: RaisedButton(
+                  child: const Text('Register'),
+                  onPressed: () {
+                    GeofencingManager.registerGeofence(
+                            GeofenceRegion(
+                                'mtv', latitude, longitude, radius, triggers,
+                                androidSettings: androidSettings),
+                            callback)
+                        .then((_) {
+                      GeofencingManager.getRegisteredGeofenceIds()
+                          .then((value) {
+                        setState(() {
+                          registeredGeofences = value;
+                        });
+                      });
+                    });
+                  },
+                ),
+              ),
+              Text('Registered Geofences: $registeredGeofences'),
+              Center(
+                child: RaisedButton(
+                  child: const Text('Unregister'),
+                  onPressed: () =>
+                      GeofencingManager.removeGeofenceById('mtv').then((_) {
+                    GeofencingManager.getRegisteredGeofenceIds().then((value) {
+                      setState(() {
+                        registeredGeofences = value;
+                      });
+                    });
+                  }),
+                ),
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Latitude',
+                ),
+                keyboardType: TextInputType.number,
+                controller: TextEditingController(text: latitude.toString()),
+                onChanged: (String s) {
+                  latitude = double.tryParse(s)!;
+                },
+              ),
+              TextField(
+                  decoration: const InputDecoration(hintText: 'Longitude'),
+                  keyboardType: TextInputType.number,
+                  controller: TextEditingController(text: longitude.toString()),
+                  onChanged: (String s) {
+                    longitude = double.tryParse(s)!;
+                  }),
+              TextField(
+                  decoration: const InputDecoration(hintText: 'Radius'),
+                  keyboardType: TextInputType.number,
+                  controller: TextEditingController(text: radius.toString()),
+                  onChanged: (String s) {
+                    radius = double.tryParse(s)!;
+                  }),
+            ],
           ),
-          body: Container(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('Current state: $geofenceState'),
-                    Center(
-                      child: RaisedButton(
-                        child: const Text('Register'),
-                        onPressed: () {
-                          if (latitude == null) {
-                            setState(() => latitude = 0.0);
-                          }
-                          if (longitude == null) {
-                            setState(() => longitude = 0.0);
-                          }
-                          if (radius == null) {
-                            setState(() => radius = 0.0);
-                          }
-                          GeofencingManager.registerGeofence(
-                              GeofenceRegion(
-                                  'mtv', latitude, longitude, radius, triggers,
-                                  androidSettings: androidSettings),
-                              callback).then((_) {
-                            GeofencingManager.getRegisteredGeofenceIds().then((value) {
-                              setState(() {
-                                registeredGeofences = value;
-                              });
-                            });
-                          });
-                        },
-                      ),
-                    ),
-                    Text('Registered Geofences: $registeredGeofences'),
-                    Center(
-                      child: RaisedButton(
-                        child: const Text('Unregister'),
-                        onPressed: () =>
-                            GeofencingManager.removeGeofenceById('mtv').then((_) {
-                              GeofencingManager.getRegisteredGeofenceIds().then((value){
-                                setState(() {
-                                  registeredGeofences = value;
-                                });
-                              });
-                            }),
-                      ),
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(
-                        hintText: 'Latitude',
-                      ),
-                      keyboardType: TextInputType.number,
-                      controller:
-                      TextEditingController(text: latitude.toString()),
-                      onChanged: (String s) {
-                        latitude = double.tryParse(s);
-                      },
-                    ),
-                    TextField(
-                        decoration:
-                        const InputDecoration(hintText: 'Longitude'),
-                        keyboardType: TextInputType.number,
-                        controller:
-                        TextEditingController(text: longitude.toString()),
-                        onChanged: (String s) {
-                          longitude = double.tryParse(s);
-                        }),
-                    TextField(
-                        decoration: const InputDecoration(hintText: 'Radius'),
-                        keyboardType: TextInputType.number,
-                        controller:
-                        TextEditingController(text: radius.toString()),
-                        onChanged: (String s) {
-                          radius = double.tryParse(s);
-                        }),
-                  ]))),
+        ),
+      ),
     );
   }
 }
